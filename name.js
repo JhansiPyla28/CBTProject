6d4d3e0026a260b9affdd3c7293fcdc44e4f56fb
JhansiPyla28 <104964403+JhansiPyla28@users.noreply.github.com>
2023-08-07 15:36:43 +0530

    Update name.js

--- name.js
+++ name.js
@@ -1,17 +1,32 @@
-<script type="text/javascript">
-SP.SOD.executeFunc('sp.js', 'SP.ClientContext', function () {
-    var context = new SP.ClientContext.get_current();
-    var currentUser = context.get_web().get_currentUser();
-    context.load(currentUser);
-    context.executeQueryAsync(
-        function() {
-            var displayName = currentUser.get_title();
-            var greetingElement = document.getElementById('greeting');
-            greetingElement.textContent = 'Hi ' + displayName;
-        },
-        function(sender, args) {
-            console.log('Failed to get user properties: ' + args.get_message());
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Personalized Greeting</title>
+    <!-- Add any CSS or other meta tags if needed -->
+</head>
+<body>
+    <div id="greeting"></div>
+
+    <script type="text/javascript">
+        function loadGreeting() {
+            SP.SOD.executeFunc('sp.js', 'SP.ClientContext', function () {
+                var context = new SP.ClientContext.get_current();
+                var currentUser = context.get_web().get_currentUser();
+                context.load(currentUser);
+                context.executeQueryAsync(
+                    function() {
+                        var displayName = currentUser.get_title();
+                        var greetingElement = document.getElementById('greeting');
+                        greetingElement.textContent = 'Hi ' + displayName;
+                    },
+                    function(sender, args) {
+                        console.log('Failed to get user properties: ' + args.get_message());
+                    }
+                );
+            });
         }
-    );
-});
-</script>
+
+        SP.SOD.executeOrDelayUntilScriptLoaded(loadGreeting, 'sp.js');
+    </script>
+</body>
+</html>
